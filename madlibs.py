1184a692c9bfe0b4308efe470e731337782cb95c
Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2024-10-23 11:45:32 +0000

    init commit

---
+++ madlibs.py
@@ -0,0 +1,31 @@
+# Collecting input from the user
+adjective1 = input("Type an adjective: ")
+animal1 = input("Type an animal: ")
+adjective2 = input("Type another adjective: ")
+animal2 = input("Type another animal: ")
+name = input("Type a name: ")
+noun1 = input("Type a noun: ")
+fantasy_place = input("Type a fantasy place: ")
+color = input("Type a color: ")
+plural_noun1 = input("Type a plural noun: ")
+adjective3 = input("Type another adjective: ")
+adjective4 = input("Type another adjective: ")
+animal3 = input("Type another animal: ")
+noun2 = input("Type another noun: ")
+adjective5 = input("Type another adjective: ")
+emotion1 = input("Type an emotion: ")
+adjective6 = input("Type another adjective: ")
+plural_noun2 = input("Type another plural noun: ")
+adjective7 = input("Type another adjective: ")
+adjective8 = input("Type another adjective: ")
+noun3 = input("Type another noun: ")
+emotion2 = input("Type another emotion: ")
+adjective9 = input("Type another adjective: ")
+animal4 = input("Type another animal: ")
+
+
+print(f"""The Adventure of the {adjective1} {animal1}
+Once upon a time, in a land far, far away, there was a {adjective2} {animal2} named {name}. One day, {name} decided to go on a journey to find the legendary {noun1} of {fantasy_place}.
+With a {color} backpack full of {plural_noun1}, they set off on a {adjective3} morning. The first stop on their adventure was the {adjective4} forest, where they met a talking {animal3} who offered them a {noun2} in exchange for a {adjective5} secret.
+Feeling {emotion1}, {name} continued their journey and crossed the {adjective6} river, where they had to swim with {plural_noun2}. Finally, they reached the entrance of the {adjective7} cave. Inside, they found the {adjective8} {noun3}, glowing in the dark.
+With {emotion2} in their heart, {name} knew that their adventure was only just beginning. Who knows where the {adjective9} {animal4} will go next?""")
